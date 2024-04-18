6249ac49990bbe6cd675821ef82d9031b7afb918
aitoyoda <tuoip164@gmail.com>
2024-04-19 00:44:24 +0900

    add firebase and change routes

---
+++ src/Route/Auth.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { AuthenticationForm } from '../Auth/AuthenticationForm';
+
+const Auth: React.FC = () => {
+  return (
+    <div>
+      <h2>Sign Up</h2>
+      <AuthenticationForm />
+    </div>
+  );
+}
+
+export default Auth;
---
+++ src/firebase.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, addDoc, getDocs } from 'firebase/firestore';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGE_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENTID,
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { app, db, collection, addDoc, getDocs,getAuth,createUserWithEmailAndPassword };
